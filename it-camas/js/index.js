101974d107c6bb7ece8971901db85cdd3251d5ed
Andrii Stratiichuk <dazzlingerino@gmail.com>
2020-12-16 23:39:08 +0200

    functionality added

---
+++ it-camas/js/index.js
@@ -0,0 +1,30 @@
+function alertValues(elPar) {
+    console.log(elPar.value);
+}
+
+function getEl(id) {
+    let elId = id;
+    let element = document.getElementById(elId);
+    return element.value;
+}
+
+
+let firstNameId = 'first-name';
+console.log(getEl(firstNameId));
+
+let lastNameId = 'last-name';
+console.log(getEl(lastNameId));
+
+let addressId = 'address';
+getEl(addressId);
+
+let citiesId = 'cities';
+getEl(citiesId);
+
+let hobbiesId = 'hobbies';
+getEl(hobbiesId);
+
+// window.alert(firstNameEl.className)
+// window.alert(lastNameEl.className)
+// window.alert(citiesEl.value)
+// window.alert(addressNameEl.className)
