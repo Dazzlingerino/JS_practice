81125ebc58fd6b2bfa5103324c7907b389d8bd57
Andrii Stratiichuk <dazzlingerino@gmail.com>
2020-12-26 16:24:09 +0200

    upgrade calc

--- it-camas/calculator/jsForCalc/calc.js
+++ it-camas/calculator/jsForCalc/calc.js
@@ -3,53 +3,33 @@ let buttonMinus = document.getElementById('buttonMinus');
 let buttonMultiply = document.getElementById('buttonMultiply');
 let buttonDivide = document.getElementById('buttonDivide');
 
+let input1 = document.getElementById('number1');
+let input2 = document.getElementById('number2');
 
-const onButtonPlusClick = () => {
-    let input1 = document.getElementById('number1');
-    let input2 = document.getElementById('number2');
-
-    let number1 = Number(input1.value);
-    let number2 = Number(input2.value);
-
-    let summ = number1 + number2;
-    alert(summ);
-
-}
-
-const onButtonMinusClick = () => {
-    let input1 = document.getElementById('number1');
-    let input2 = document.getElementById('number2');
+const onButtonClick = (eventObject) => {
+    debugger;
+    let clickedElement = eventObject.currentTarget;
+    let operationSign = clickedElement.innerHTML;
 
     let number1 = Number(input1.value);
     let number2 = Number(input2.value);
+    let result = document.getElementById("result-field")
 
-    let diff = number1 - number2;
-    alert(diff);
+    if (operationSign == buttonPlus.innerHTML) {
+        result.value = number1 + number2;
+    }
+    else if (operationSign == buttonMinus.innerHTML) {
+        result.value = number1 - number2;
+    }
+    else if (operationSign == buttonMultiply.innerHTML) {
+        result.value = number1 * number2;
+    }
+    else if (operationSign == buttonDivide.innerHTML) {
+        result.value = number1 / number2;
+    }
 }
 
-const onButtonMultiplyClick = () => {
-    let input1 = document.getElementById('number1');
-    let input2 = document.getElementById('number2');
-
-    let number1 = Number(input1.value);
-    let number2 = Number(input2.value);
-
-    let mult = number1 * number2;
-    alert(mult);
-}
-
-const onButtonDivideClick = () => {
-    let input1 = document.getElementById('number1');
-    let input2 = document.getElementById('number2');
-
-    let number1 = Number(input1.value);
-    let number2 = Number(input2.value);
-
-    let divi = number1 / number2;
-    alert(divi);
-}
-
-buttonPlus.addEventListener('click', onButtonPlusClick);
-buttonMinus.addEventListener('click', onButtonMinusClick);
-buttonMultiply.addEventListener('click', onButtonMultiplyClick);
-buttonDivide.addEventListener('click', onButtonDivideClick);
+buttonPlus.addEventListener('click', onButtonClick);
+buttonMinus.addEventListener('click', onButtonClick);
+buttonMultiply.addEventListener('click', onButtonClick);
+buttonDivide.addEventListener('click', onButtonClick);
